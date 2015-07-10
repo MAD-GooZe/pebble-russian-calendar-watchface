097c402e8f4220f506283a43ccde3adda7b61dc8
MAD-GooZe <gusev@aesc.msu.ru>
2015-07-10 03:01:48 +0300

    testing js

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,10 +1,9 @@
 Pebble.addEventListener("ready",
     function(e) {
-
+        Pebble.addEventListener('showConfiguration', function(e) {
+            // Show config page
+            Pebble.openURL('http://mad-gooze.github.io/pebble-russian-calendar-watchface/config.html');
+        });
     }
 );
 
-Pebble.addEventListener('showConfiguration', function(e) {
-    // Show config page
-    Pebble.openURL('ya.ru');
-});
